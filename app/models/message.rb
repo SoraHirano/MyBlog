f81f1f871ec266acfb206cc1e7fff62f548db2dd
Sora <shirano1206@gmail.com>
2017-07-01 09:35:50 +0000

    Add root

--- app/models/message.rb
+++ app/models/message.rb
@@ -1,7 +1,2 @@
 class Message < ApplicationRecord
- validates :title , length: { maximum: 32 } , presence: true
- validates :message , length: { maximum: 256 } , presence: true
- validates :name , length: { maximum: 32 }
-
- validates :cost ,  presence: true
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,12 @@
 Rails.application.routes.draw do
   resources :messages
+  get '/messages/new' => "messages#new"
+  get '/messages' => 'messages#index'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
+Rails.application.routes.draw do
+ root 'messages#index'
+ resources :messages
+ # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+end
+root
